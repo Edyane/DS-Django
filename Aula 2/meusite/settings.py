7eedb102e1e3ff6e7c703e24beb954b7275528f0
Edyane <edyanealves@gmail.com>
2022-12-06 20:28:47 -0300

    Adicionando Arquivo das Aulas

--- Aula 2/meusite/settings.py
+++ Aula 2/meusite/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f_ijyn6pcr3%747jlz!+sp202k&*)m!w&@#av_&yb-+^2m21rh'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
